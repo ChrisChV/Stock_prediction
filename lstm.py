60acea5e72a29b2a2b2a55693eba335ff33184e7
ChrisChV <xnpiochv@gmail.com>
2019-05-17 14:34:38 -0500

    lstm added

---
+++ lstm.py
@@ -0,0 +1,59 @@
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
+import db_functions
+
+
+data = db_functions.getDb()
+
+data.index = data.Date
+data.drop('Date', axis=1, inplace=True)
+
+
+dataset = data.values
+
+train = dataset[0:987,:]
+valid = dataset[987:,:]
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(dataset)
+
+
+x_train, y_train = [], []
+for i in range(60,len(train)):
+    x_train.append(scaled_data[i-60:i,0])
+    y_train.append(scaled_data[i,0])
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+
+x_train = np.reshape(x_train, (x_train.shape[0],x_train.shape[1],1))
+
+model = Sequential()
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1],1)))
+model.add(LSTM(units=50))
+model.add(Dense(1))
+
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(x_train, y_train, epochs=50, batch_size=1, verbose=2)
+
+inputs = data[len(data) - len(valid) - 60:].values
+inputs = inputs.reshape(-1,1)
+inputs  = scaler.transform(inputs)
+
+X_test = []
+for i in range(60,inputs.shape[0]):
+    X_test.append(inputs[i-60:i,0])
+X_test = np.array(X_test)
+
+X_test = np.reshape(X_test, (X_test.shape[0],X_test.shape[1],1))
+closing_price = model.predict(X_test)
+closing_price = scaler.inverse_transform(closing_price)
+
+train = data[:987]
+valid = data[987:]
+
+#print(db_functions.getRMSE(valid, closing_price))
+
+db_functions.plot(train, valid, closing_price)
+
